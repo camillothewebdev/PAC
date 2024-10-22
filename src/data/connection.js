e10d460ad522bdb88536a5e171f62b997bc4816b
camillothewebdev <camillothewebdev@gmail.com>
2024-10-22 20:16:47 +0200

    Update connection.js

--- src/data/connection.js
+++ src/data/connection.js
@@ -1,6 +1,7 @@
-export default "https://api-patriote-en-action.onrender.com";
+export default "https://api-patriote-en-action.onrender.com/";
 
 /*
 https://api-tutored-projet.onrender.com
+https://api-patriote-en-action.onrender.com/
 http://localhost:8800
 */
